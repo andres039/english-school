e441324bd245e4b0a2dfd5602cf395ace45c280e
Andres <andres039039@hotmail.com>
2022-10-27 23:23:21 -0400

    fix layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -83,38 +83,42 @@ const Home: NextPage = () => {
         }}
       >
         <Grid container spacing={2} sx={{ minHeight: "60vh", p: 4 }}>
-        <Slide in={true} direction="right" timeout={1000}>
-          <Grid
-            sm={12}
-            md={9}
-            sx={{
-              maxHeight: "70vh",
-              maxWidth: "100vw",
-            }}
-          >
-            <Image
-              alt="Books and a map of the world"
-              src={backGround}
-              height={1100}
-            />
-          </Grid>
+          <Slide in={true} direction="right" timeout={1000}>
+            <Grid
+              sm={12}
+              md={9}
+              sx={{
+                maxHeight: "70vh",
+                maxWidth: "100vw",
+              }}
+            >
+              <Image
+                alt="Books and a map of the world"
+                src={backGround}
+                height={1100}
+              />
+            </Grid>
           </Slide>
           <Grid sm={12} md={3}>
             <Grid container>
-        <Slide in={true} direction="left" timeout={1000}>
-
-              <Grid sx={{ p: 4 }} xs={12}>
-                <Typography variant="h6" color="A100">
-                  Aprende Inglés en La Mesa Cundinamarca con los mejores métodos
-                  de las escuelas australianas.
-                </Typography>
-              </Grid>
+              {/* <Slide in={true} direction="left" timeout={1000}> */}
+                <Grid sx={{ p: 4 }} xs={12}>
+                  <Typography variant="h6" color="A100">
+                    Aprende Inglés en La Mesa Cundinamarca con los mejores
+                    métodos de las escuelas australianas.
+                  </Typography>
+                </Grid>
+              {/* </Slide> */}
+              <Slide in={true} direction="up" timeout={1000}>
+                <Grid xs={12} container justifyContent="center">
+                  <Image
+                    alt="Kangaroo"
+                    src={kangaroo}
+                    height={100}
+                    width={100}
+                  />
+                </Grid>
               </Slide>
-                <Slide in={true} direction="up" timeout={1000}>
-              <Grid xs={12} container justifyContent="center">
-                <Image alt="Kangaroo" src={kangaroo} height={100} width={100} />
-              </Grid>
-                </Slide>
             </Grid>
           </Grid>
         </Grid>
