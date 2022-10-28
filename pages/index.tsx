728fb4d2b08d656b3d01adfe4240998166cbedb8
Andres <andres039039@hotmail.com>
2022-10-27 23:15:28 -0400

    adds animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 import {
   AppBar,
+  Box,
   Button,
   Divider,
   Fade,
+  IconButton,
   Link,
   Slide,
+  Toolbar,
   Typography,
   Unstable_Grid2 as Grid,
   Zoom,
@@ -29,63 +32,58 @@ const Home: NextPage = () => {
       <Head>
         <title>English School Center</title>
       </Head>
-      <AppBar sx={{ p: 2, backgroundColor: backColor }}>
-        <Stack
-          direction="row"
-          justifyContent="space-between"
-          alignItems="center"
-        >
-          <Zoom in={true} timeout={1000}>
-            <Grid container sx={{ width: "25vw" }}>
-              <Grid sm={12} md={4}>
-                <Typography align="center" color="blue" variant="h5">
+      <AppBar position="static" sx={{ backgroundColor: backColor }}>
+        <Toolbar>
+          <Stack
+            direction="row"
+            justifyContent="space-between"
+            alignItems="center"
+            sx={{ width: "100vw" }}
+          >
+            <Zoom in={true} timeout={1000}>
+              <Stack direction="row" spacing={1}>
+                <Typography color="blue" variant="h5">
                   English
                 </Typography>
-              </Grid>
-              <Grid sm={12} md={4}>
-                <Typography
-                  align="center"
-                  color="red"
-                  variant="h5"
-                  component="div"
-                >
+                <Typography color="red" variant="h5">
                   School
                 </Typography>
-              </Grid>
-              <Grid sm={12} md={4}>
-                <Typography align="center" color="white" variant="h5">
+                <Typography color="white" variant="h5">
                   Center
                 </Typography>
-              </Grid>
-            </Grid>
-          </Zoom>
+              </Stack>
+            </Zoom>
 
-          <Stack direction="row" justifySelf="flex-end" sx={{ pr: 4 }} spacing={2}>
-            <Fade in={true} timeout={1000}>
-              <Link href="mailto:englishschoolcenteresc@gmail.com">
-                <Button variant="outlined">
+            {/* <Stack direction="row" justifySelf="flex-end" sx={{ pr: 4 }} spacing={2}> */}
+            <Stack direction="row">
+              {/* <Fade in={true} timeout={1000}> */}
+              <IconButton edge="end" size="large">
+                <Link href="mailto:englishschoolcenteresc@gmail.com">
                   <MailIcon />
-                </Button>
-              </Link>
-            </Fade>
-            <Fade in={true} timeout={2000}>
-              <Link href="https://www.facebook.com/englishschoolcenter/">
-                <Button variant="outlined">
+                </Link>
+              </IconButton>
+              {/* </Fade> */}
+              {/* <Fade in={true} timeout={2000}> */}
+              <IconButton size="large">
+                <Link href="https://www.facebook.com/englishschoolcenter/">
                   <FacebookIcon />
-                </Button>
-              </Link>
-            </Fade>
+                </Link>
+              </IconButton>
+            </Stack>
+            {/* </Fade> */}
+            {/* </Stack> */}
           </Stack>
-        </Stack>
+        </Toolbar>
       </AppBar>
       <Container
         sx={{
           maxHeight: "100vh",
           maxWidth: "100vw",
-          mt: "15vh",
+          mt: "5vh",
         }}
       >
         <Grid container spacing={2} sx={{ minHeight: "60vh", p: 4 }}>
+        <Slide in={true} direction="right" timeout={1000}>
           <Grid
             sm={12}
             md={9}
@@ -95,22 +93,28 @@ const Home: NextPage = () => {
             }}
           >
             <Image
-              alt="The house from the offer."
+              alt="Books and a map of the world"
               src={backGround}
               height={1100}
             />
           </Grid>
+          </Slide>
           <Grid sm={12} md={3}>
             <Grid container>
+        <Slide in={true} direction="left" timeout={1000}>
+
               <Grid sx={{ p: 4 }} xs={12}>
                 <Typography variant="h6" color="A100">
                   Aprende Inglés en La Mesa Cundinamarca con los mejores métodos
                   de las escuelas australianas.
                 </Typography>
               </Grid>
+              </Slide>
+                <Slide in={true} direction="up" timeout={1000}>
               <Grid xs={12} container justifyContent="center">
                 <Image alt="Kangaroo" src={kangaroo} height={100} width={100} />
               </Grid>
+                </Slide>
             </Grid>
           </Grid>
         </Grid>
@@ -120,17 +124,14 @@ const Home: NextPage = () => {
             direction="row"
             spacing={2}
             justifyContent="center"
-            sx={{ mt: 3 }}
+            sx={{ mt: 6 }}
           >
-            <Fade in={true} timeout={3000}>
-              <PhoneIcon fontSize="large" color="action" />
-            </Fade>
-            <Typography align="center" variant="h4" color="grey">
+            <Typography align="center" variant="h5" color="grey">
               {" "}
               Contáctanos: 3172407551
             </Typography>
           </Stack>
-          <Typography align="center" variant="h4" color="grey">
+          <Typography align="center" variant="h5" color="grey">
             {" "}
             Visítanos: Cl. 8, La Mesa, Cundinamarca, Colombia
           </Typography>
